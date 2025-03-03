8297470d34129d8af73c179b85951789371cbd2b
Mofe Ogunkanmi <mofe@peachlovesdigital.co.uk>
2025-03-03 17:42:44 +0000

    links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css";
-import "https://unicons.iconscout.com/release/v2.1.9/css/unicons.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +19,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css"
+      />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
